2e756542538e0c7da6c5c198568eaf01d48134b2
fimovol <fimovolpe@gmail.com>
2022-01-14 11:13:47 -0500

    agregando NoticiasLaRepublica

--- servidor.js
+++ servidor.js
@@ -26,6 +26,7 @@ var schema = buildSchema(`
     animeflash: [Anime]
     noticias(limit: Int): [Noticia]
     peru21noticias(limit: Int): [Noticia]
+    larepublica(limit: Int): [Noticia]
   }
 `);
 
@@ -147,6 +148,38 @@ var root = {
     await browser.close();
     return enlaces.slice(0,limit)
   },
+  larepublica: async({limit}) => {
+    const goto = 'https://larepublica.pe/ultimas-noticias/';
+    const browser = await puppeteer.launch(/*{headless:false}*/);
+    const page = await browser.newPage();
+    await page.goto(goto);
+    for(let i = 0; i < 3; i++){
+        await page.evaluate(() => {
+            window.scrollBy(0, window.innerHeight);
+        })
+        await page.waitForTimeout(500)
+    } 
+    const enlaces = await page.evaluate(() => {
+        const elements = document.querySelectorAll('ul li:nth-child(2) [class="PostSectionListH3"] [class="PostSectionListA"] ')
+        const elements2 = document.querySelectorAll('ul li:nth-child(1) [class="PostSectionListSPAN"] ')
+        const elements3 = document.querySelectorAll('div div:nth-child(1) a span img')
+
+        const links = []
+
+        elements3.forEach( (e,p) => {
+            const objeto = {
+                nombre: elements[p].textContent,
+                hora: elements2[p].textContent,
+                img: e.getAttribute('src'),
+                href: 'https://larepublica.pe'+elements[p].getAttribute('href')
+            }
+            links.push(objeto)
+        })
+        return links;
+    })
+    await browser.close();
+    return enlaces.slice(0,limit)
+  }
 }
 
 var app = express();
