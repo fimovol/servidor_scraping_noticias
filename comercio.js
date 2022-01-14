dffed9753a71d3cd24a5d1b48e60e6fc074b28b8
fimovol <fimovolpe@gmail.com>
2022-01-14 11:13:58 -0500

    modificaciones de prueba

--- comercio.js
+++ comercio.js
@@ -1,28 +1,34 @@
 const puppeteer=require('puppeteer');
 (async()=>{
-    const goto = 'https://elcomercio.pe/ultimas-noticias/';
+    const goto = 'https://larepublica.pe/ultimas-noticias/';
     const browser = await puppeteer.launch(/*{headless:false}*/);
     const page = await browser.newPage();
     await page.goto(goto);
-    await page.waitForTimeout(2000)
+    for(let i = 0; i < 3; i++){
+        await page.evaluate(() => {
+            window.scrollBy(0, window.innerHeight);
+        })
+        await page.waitForTimeout(500)
+    } 
     const enlaces = await page.evaluate(() => {
-        const elements = document.querySelectorAll('[role="main"] div [itemprop="name"] a');
-        const elements2 = document.querySelectorAll('[role="main"] div [class="story-item__top flex items-center md:flex-col md:items-start"] p');
-        const elements3 = document.querySelectorAll('[role="main"] div div div figure a picture img');
+        const elements = document.querySelectorAll('ul li:nth-child(2) [class="PostSectionListH3"] [class="PostSectionListA"] ')
+        const elements2 = document.querySelectorAll('ul li:nth-child(1) [class="PostSectionListSPAN"] ')
+        const elements3 = document.querySelectorAll('div div:nth-child(1) a span img')
 
         const links = []
 
-        elements.forEach( (e,p) => {
+        elements3.forEach( (e,p) => {
             const objeto = {
-                nombre: e.textContent,
+                nombre: elements[p].textContent,
                 hora: elements2[p].textContent,
-                img: elements3[p].getAttribute('src')
+                img: e.getAttribute('src'),
+                href: 'https://larepublica.pe'+elements[p].getAttribute('href')
             }
             links.push(objeto)
         })
         return links;
     })
-    console.log(enlaces)
+    console.log(enlaces,enlaces.length)
     
     await browser.close();
 })();
